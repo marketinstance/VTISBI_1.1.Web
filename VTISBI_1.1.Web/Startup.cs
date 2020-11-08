efdecedbb0e910d0e99df96072a994145e9d2b26
marketinstance <71776991+marketinstance@users.noreply.github.com>
2020-11-08 17:13:50 +0300

    2 часть от Гоши Дударь

--- VTISBI_1.1.Web/Startup.cs
+++ VTISBI_1.1.Web/Startup.cs
@@ -24,6 +24,7 @@ namespace VTISBI_1._1.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
